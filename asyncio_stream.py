f2e5e890114de956ba542cff3de645a23cc52f09
Florian Aubermann <florian.aubermann@mr.mpg.de>
2024-01-25 18:59:05 +0100

    changes on PC

--- asyncio_stream.py
+++ asyncio_stream.py
@@ -9,27 +9,29 @@ async def send_zmq_request(interval, odin):
     while True:
         start_time = time.time()
         # Replace 'your_request_data' with the actual data you want to send
-        response = odin.stream_get()
+        response = odin.stream_get(mode=None)
 
-        # Wait for the response (you may adjust this based on your application)
-        print(f"Received response: {response}")
-        new_time = time.time()
-        print(new_time - start)
-        start = new_time
+        # Wait for the response
+        #print(f"Received response: {response}", len(response))
+        #new_time = time.time()
+        #print(new_time - start)
+        #start = new_time
 
         end_time = time.time()
         execution_time = end_time - start_time
+        print(execution_time)
 
         # Sleep for the specified interval
-        await asyncio.sleep(max(0, interval - execution_time))
+        #await asyncio.sleep(max(0, interval - execution_time))
 
 
 if __name__ == "__main__":
     odin = OdinZMQ("172.16.19.108")
     odin.stream_configure('all')
+    time.sleep(5)
 
     # Set the interval in seconds (5 ms = 0.005 seconds)
-    interval_seconds = 0.004
+    interval_seconds = 0.0
     start = time.time()
 
     # Create an event loop
